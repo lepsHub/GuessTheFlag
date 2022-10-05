45b11571bcbfb2eb990a8dd73e13967cbcbe9e47
Luis Enrique <backup.user.leps@outlook.com>
2022-10-05 13:20:36 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Luz Nunez on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
